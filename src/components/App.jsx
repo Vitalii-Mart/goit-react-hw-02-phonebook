ec0145e7b15570585e7e7c5b375e373477593d36
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-04-22 21:30:24 +0300

    completed the task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import { Section, Title } from './App.styled';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    if (contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      const cont = {
+        id: shortid.generate(),
+        name,
+        number,
+      };
+      this.setState(prevState => ({
+        contacts: [cont, ...prevState.contacts],
+      }));
+    }
+  };
+
+  changeFiltar = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  getFilter = () => {
+    const { filter, contacts } = this.state;
+    const normalFiltar = filter.toLowerCase();
+
+    return contacts.filter(cont =>
+      cont.name.toLowerCase().includes(normalFiltar)
+    );
+  };
+  onDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    const filterContacts = this.getFilter();
+    return (
+      <Section>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.addContact} />
+
+        <Title>Contacts</Title>
+        <Filter onChange={this.changeFiltar} value={filter} />
+        <ContactList
+          contacts={filterContacts}
+          onDeleteContact={this.onDeleteContact}
+        />
+      </Section>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ElemContactList/ElemContactList.styled.jsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const Items = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 5px;
+  padding: 5px;
+  color: rgb(47, 47, 226);
+`;
+
+export const Caption = styled.p`
+  color: rgb(47, 47, 226);
+`;
+
+export const Button = styled.button`
+  margin-left: auto;
+  padding: 5px;
+  color: rgb(47, 47, 226);
+  border-radius: 4px;
+`;
